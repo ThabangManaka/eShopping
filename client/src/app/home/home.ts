c662d227956704bcfe88138682cb6d01e761f66a
Thabang Manaka <thabangdansteyn@gmail.com>
2026-08-03 13:25:52 +0200

    Added Carousel on home page

--- client/src/app/home/home.ts
+++ client/src/app/home/home.ts
@@ -1,9 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule,
+            NgbCarouselModule],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
-export class Home {}
+export class Home {
+    onSlide(event: any) {
+    console.log(event);
+  }
+}
