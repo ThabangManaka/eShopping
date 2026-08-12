9732ea6a5d9db6e7523d40ab78beed3aa574e02d
Thabang Manaka <thabangdansteyn@gmail.com>
2026-08-12 19:33:17 +0200

    Added loader

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./core/navbar/navbar";
 import { Header } from "./core/header/header";
+import { NgxSpinnerComponent } from 'ngx-spinner';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, Header],
+  imports: [RouterOutlet, NavbarComponent, Header,NgxSpinnerComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
