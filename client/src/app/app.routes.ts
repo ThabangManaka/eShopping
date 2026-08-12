3219686f7384746727bdc0b500ef1760609bdf43
Thabang Manaka <thabangdansteyn@gmail.com>
2026-08-12 19:14:27 +0200

    Added Not found page

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { Home } from './home/home';
+import { NotFound } from './core/not-found/not-found';
 
 
 export const routes: Routes = [
      {path: '',  component:Home, data:{breadcrumb:'Home'}},
+     {path: 'not-found', component: NotFound},
      { path: 'store',loadComponent: () =>import('./store/store').then(c => c.Store),data: { breadcrumb: 'Store' } },
       {
     path: 'store/:id',loadComponent: () =>
@@ -13,5 +15,5 @@ export const routes: Routes = [
     { path: 'basket',loadComponent: () =>import('./basket/basket').then(c => c.Basket),data: { breadcrumb: 'basket' } }, 
     { path: 'checkout',loadComponent: () =>import('./checkout/checkout').then(c => c.Checkout),data: { breadcrumb: 'basket' } }, 
     { path: 'login',loadComponent: () =>import('./account/login/login').then(c => c.Login),data: { breadcrumb: 'login' } }, 
-
+    {path: '**', redirectTo: '', pathMatch: 'full'},
 ];
